c40bd8749df3dea095433df8fbce375c72a60060
Martin Proks <mproksik@gmail.com>
2018-10-11 12:32:12 +0200

    Removed test.sh

--- test/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run pipeline
-nextflow run NGI-RNAfusion -profile test --reads "/test-data/test_{1,2}.fastq.gz" --genome R64-1-1 --star_fusion
-
-# Test per tool
-# cp -r /results/* NGI-RNAfusion/results
