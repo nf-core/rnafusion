6adb59d1806d984b498c61ea4322c38befbe7bec
Martin Proks <mproksik@gmail.com>
2018-10-12 14:09:49 +0200

    Moved download inside travis

--- test/build.sh
+++ test/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 cp ../environment.yml .
+wget ftp://ftp.ensembl.org/pub/current_fasta/saccharomyces_cerevisiae/dna/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa.gz -O data/genome.fa.gz
+wget ftp://ftp.ensembl.org/pub/release-94/gtf/saccharomyces_cerevisiae/Saccharomyces_cerevisiae.R64-1-1.94.gtf.gz -O data/annotation.gtf.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz -O data/Pfam-A.hmm.gz
 docker build . -t "rnafusion-test"
