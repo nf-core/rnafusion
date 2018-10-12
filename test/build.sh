5d19ac3d5803409b822545c0f8125251e2eacb8e
Martin Proks <mproksik@gmail.com>
2018-10-12 16:54:00 +0200

    Created fake volume container to speed up the process
    Pretty smart actually

--- test/build.sh
+++ test/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 cp ../environment.yml .
-# Get Pfam and test data
-wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz -O data/Pfam-A.hmm.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR612/006/SRR6129616/SRR6129616_1.fastq.gz -O test-data/test_1.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR612/006/SRR6129616/SRR6129616_2.fastq.gz -O test-data/test_2.fastq.gz
-# Build docker
 docker build . -t "rnafusion-test"
