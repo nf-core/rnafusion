0cdf1e0fb9ada9491388469083a08f3a3d78a3d2
Martin Proks <mproksik@gmail.com>
2018-10-12 16:02:04 +0200

    Travis, work with me here

--- test/build.sh
+++ test/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 cp ../environment.yml .
-mkdir data/
-wget ftp://ftp.ensembl.org/pub/current_fasta/saccharomyces_cerevisiae/dna/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa.gz -O data/genome.fa.gz
-wget ftp://ftp.ensembl.org/pub/release-94/gtf/saccharomyces_cerevisiae/Saccharomyces_cerevisiae.R64-1-1.94.gtf.gz -O data/annotation.gtf.gz
+# Get Pfam and test data
 wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz -O data/Pfam-A.hmm.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR612/006/SRR6129616/SRR6129616_1.fastq.gz -O test-data/test_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR612/006/SRR6129616/SRR6129616_2.fastq.gz -O test-data/test_2.fastq.gz
+# Build docker
 docker build . -t "rnafusion-test"
