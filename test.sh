63fda22959f5535d62cd2580c71b6fcdc9cf2572
Martin Proks <mproksik@gmail.com>
2020-01-30 11:56:19 +0100

    fix: updated paths for internal testing

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Test tools only
-nextflow run NGI-RNAfusion/ -profile munin -c munin-singularity-1.1.0.config --reads '/data1/references/rnafusion/1.1.0/reads_{1,2}.fq.gz' --arriba --star_fusion --fusioncatcher --ericscript --pizzly --squid --debug
+nextflow run NGI-RNAfusion/ -profile munin -c '/data1/references/rnafusion/1.1.0/munin-singularity.config' --reads '/data1/references/rnafusion/1.1.0/reads_{1,2}.fq.gz' --arriba --star_fusion --fusioncatcher --ericscript --pizzly --squid --debug
 
 # Test batch mode
-nextflow run NGI-RNAfusion/ -profile munin -c munin-singularity-1.1.0.config --reads '/data1/references/rnafusion/1.1.0/batch_data/00*/*_R{1,2}.fq.gz' --arriba --star_fusion --fusioncatcher --ericscript --pizzly --squid
+nextflow run NGI-RNAfusion/ -profile munin -c '/data1/references/rnafusion/1.1.0/munin-singularity.config' --reads '/data1/references/rnafusion/1.1.0/batch_data/00*/*_R{1,2}.fq.gz' --arriba --star_fusion --fusioncatcher --ericscript --pizzly --squid
 
 # Test the whole pipeline
-nextflow run NGI-RNAfusion/ -profile munin -c munin-singularity-1.1.0.config --reads '/data1/matq007/data/reads_{1,2}.fq.gz' --arriba --star_fusion --fusioncatcher --ericscript --pizzly --squid --arriba_vis --fusion_inspector
+nextflow run NGI-RNAfusion/ -profile munin -c '/data1/references/rnafusion/1.1.0/munin-singularity.config' --reads '/data1/references/rnafusion/1.1.0/reads_{1,2}.fq.gz' --arriba --star_fusion --fusioncatcher --ericscript --pizzly --squid --arriba_vis --fusion_inspector
