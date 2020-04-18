0f147cb6d56b7f3d5d9a208cb5d51a2a27c9939e
Martin Proks <mproksik@gmail.com>
2020-04-18 10:51:14 +0200

    fix: simplified

--- utils/download-singularity-img.sh
+++ utils/download-singularity-img.sh
@@ -23,15 +23,12 @@ elif [ -d "$1" ]; then
     FUSIONCATCHER=$(get_tool_version "fusioncatcher")
     FUSION_INSPECTOR=$(get_tool_version "fusion_inspector")
     PIZZLY=$(get_tool_version "pizzly")
-    RNAFUSION="$(cat ../nextflow.config | grep "container" | cut -d":" -f2 | cut -d "'" -f1)"
+    RNAFUSION="$(cat ../nextflow.config | grep -m1 "container" | cut -d":" -f2 | cut -d "'" -f1)"
     SQUID=$(get_tool_version "squid")
     STAR_FUSION=$(get_tool_version "star_fusion")
-    
-    cwd=$1/rnafusion_containers_v$RNAFUSION
-    echo "Creating folder $cwd"
-    mkdir $cwd && cd $cwd
-    echo "Pulling images ..."
-    
+
+    cd $1 && echo "Pulling images ..."
+
     singularity pull --name "rnafusion_arriba_v${ARRIBA}.img" docker://nfcore/rnafusion:arriba_v${ARRIBA}
     singularity pull --name "rnafusion_ericscript_v${ERICSCRIPT}.img" docker://nfcore/rnafusion:ericscript_v${ERICSCRIPT}
     singularity pull --name "rnafusion_fusioncatcher_v${FUSIONCATCHER}.img" docker://nfcore/rnafusion:fusioncatcher_v${FUSIONCATCHER}
@@ -40,7 +37,7 @@ elif [ -d "$1" ]; then
     singularity pull --name "rnafusion_v${RNAFUSION}.img" docker://nfcore/rnafusion:${RNAFUSION}
     singularity pull --name "rnafusion_squid_v${SQUID}.img" docker://nfcore/rnafusion:squid_v${SQUID}
     singularity pull --name "rnafusion_star-fusion_v${STAR_FUSION}.img" docker://nfcore/rnafusion:star-fusion_v${STAR_FUSION}
-    
+
 else
     echo "Path doesn't exists"
     exit 1
