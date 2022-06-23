95b1cdeb66191bf21bd48c2a6cb6a383c7b013e1
Annick Renevey <47788523+rannick@users.noreply.github.com>
2022-06-23 15:53:55 +0200

    adapt pipeline name

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -7,7 +7,7 @@ import argparse
 
 
 def parse_args(args=None):
-    Description = "Reformat nf-core/rnaseq samplesheet file and check its contents."
+    Description = "Reformat nf-core/rnafusion samplesheet file and check its contents."
     Epilog = "Example usage: python check_samplesheet.py <FILE_IN> <FILE_OUT>"
 
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
